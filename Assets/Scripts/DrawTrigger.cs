6f19a645c9d6c86f76ff35e5de6c72a602a4f7ee
harl-i <ilya.kharlamov.92@mail.ru>
2023-04-12 08:10:27 +0300

    Add DrawTrigger and LevelEndTrigger
    Add DrawTrigger script and LevelEndTrigger gameobject

---
+++ Assets/Scripts/DrawTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTrigger : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D _boxCollider2D;
+    [SerializeField] private Color _color;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+
+        Vector2 triggerPosition = new Vector2(transform.position.x, transform.position.y);
+
+        Gizmos.DrawCube(triggerPosition + _boxCollider2D.offset, _boxCollider2D.size);
+    }
+}
