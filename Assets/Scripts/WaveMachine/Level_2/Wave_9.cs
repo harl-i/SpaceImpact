47eab860658b89a00daa05496b919266544bf3e2
harl-i <ilya.kharlamov.92@mail.ru>
2023-04-23 00:03:04 +0300

    complete Wave_9 LvL2

---
+++ Assets/Scripts/WaveMachine/Level_2/Wave_9.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave_9 : Wave
+{
+    private int _enemiesCountOnWave = 5;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, _enemiesCountOnWave, _spawnPoints[0].transform.position, MoveVariant));
+    }
+}
