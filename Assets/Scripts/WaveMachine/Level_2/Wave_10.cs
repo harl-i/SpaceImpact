7f7b34a8a26242c54c80ad46da2411118d92e057
harl-i <ilya.kharlamov.92@mail.ru>
2023-04-23 14:52:07 +0300

    complete Wave_11 LvL2

---
+++ Assets/Scripts/WaveMachine/Level_2/Wave_10.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave_10 : Wave
+{
+    private int _enemiesCountOnIteration = 3;
+    private int _iterations = 3;
+
+    private void OnEnable()
+    {
+        StartCoroutine(StartWave());
+    }
+
+    private IEnumerator StartWave()
+    {
+        for (int i = 0; i < _iterations; i++)
+        {
+            for (int j = 0; j < _enemiesCountOnIteration; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    StartCoroutine(SpawnEnemy(_enemysPool, 1.7f, 1, _spawnPoints[j].transform.position, MoveVariant));
+                }
+                else
+                {
+                    StartCoroutine(SpawnEnemy(_enemysPool, 0, 1, _spawnPoints[j].transform.position, MoveVariant));
+                }
+            }
+
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
