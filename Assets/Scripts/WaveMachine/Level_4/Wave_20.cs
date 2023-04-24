ce6957044255c23601884e8206da1a960ca624e2
harl-i <ilya.kharlamov.92@mail.ru>
2023-04-24 20:47:50 +0300

    complete Wave_20 LvL4

---
+++ Assets/Scripts/WaveMachine/Level_4/Wave_20.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave_20 : Wave
+{
+    private void OnEnable()
+    {
+        StartCoroutine(StartWave());
+    }
+
+    private IEnumerator StartWave()
+    {
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, 1, _spawnPoints[1].transform.position, MoveVariant));
+
+        yield return new WaitForSeconds(5);
+
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, 2, _spawnPoints[0].transform.position, MoveVariant));
+        
+        yield return new WaitForSeconds(5);
+
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, 2, _spawnPoints[2].transform.position, MoveVariant));
+
+        yield return new WaitForSeconds(5);
+
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, 3, _spawnPoints[1].transform.position, MoveVariant));
+    }
+}
