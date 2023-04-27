141830df4a8fcfbbe9c645e0056fb1d5b3bfec51
harl-i <ilya.kharlamov.92@mail.ru>
2023-04-27 03:06:45 +0300

    Complete LvL4

---
+++ Assets/Scripts/WaveMachine/Level_4/WaveBoss_4.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBoss_4 : Wave
+{
+    [SerializeField] private List<GameObject> _wayPoints = new List<GameObject>();
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnEnemy(_enemysPool, _spawnDelay, 1, _spawnPoints[0].transform.position, _wayPoints, true));
+    }
+}
