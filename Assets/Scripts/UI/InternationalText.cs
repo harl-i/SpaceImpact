66f86bb7b83881d6b76431cd7136b780fea23733
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2023-11-09 03:13:54 +0300

    Localization completed

---
+++ Assets/Scripts/UI/InternationalText.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class InternationalText : MonoBehaviour
+{
+    [SerializeField] private string _en;
+    [SerializeField] private string _ru;
+    [SerializeField] private string _tr;
+
+    private TextMeshProUGUI _textMeshProUGUI;
+
+    private const string Ru = "ru";
+    private const string Tr = "tr";
+
+    private void Awake()
+    {
+        _textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        if (Language.Instance.CurrentLanguage == Tr)
+        {
+            _textMeshProUGUI.text = _tr;
+        }
+        else if (Language.Instance.CurrentLanguage == Ru)
+        {
+            _textMeshProUGUI.text = _ru;
+        }
+        else
+        {
+            _textMeshProUGUI.text = _en;
+        }
+    }
+}
